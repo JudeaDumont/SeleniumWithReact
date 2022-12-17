f4266ef61dd5e6ff9634969b9a45a362bf8cea79
JudeaDumont <dumontjudea2@gmail.com>
2022-12-16 18:14:37 -0700

    Navigate to react successful

--- src/test/java/pages/steps/LoginStep.java
+++ src/test/java/pages/steps/LoginStep.java
@@ -7,11 +7,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import model.Candidate;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.CandidatePage;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +16,8 @@ import java.util.Map;
 import static util.util.getCandidateFromTableDiff;
 
 public class LoginStep extends BaseUtil {
+
+    private final String urlBase = "http://localhost:3000";
 
     private final List<List<Candidate>> candidateTableHistory
             = new ArrayList<>();
@@ -37,111 +36,12 @@ public class LoginStep extends BaseUtil {
         );
     }
 
-    @Given("^I navigate to Angular$")
+    @Given("^I navigate to React")
     public void iNavigateToAngular() throws Throwable {
         scenarioDef.createNode(
                 new GherkinKeyword("Given"),
-                "I navigate to Angular");
-        Driver.navigate().to("http://localhost:4200");
+                "I navigate to React");
+        Driver.navigate().to(urlBase);
 
     }
-
-    @And("^I add candidate table to history$")
-    public void iAddCandidateToTableHistory() throws ClassNotFoundException {
-        scenarioDef.createNode(
-                new GherkinKeyword("And"),
-                "I add candidate table to history");
-        CandidatePage page = new CandidatePage(Driver);
-
-        //List<Candidate> candidatesFromTable = page.getCandidatesFromTable();
-        List<Candidate> candidatesFromTable = forceCandidateTableDelta(page);
-
-        candidateTableHistory.add(candidatesFromTable);
-    }
-
-    private List<Candidate> forceCandidateTableDelta(
-            CandidatePage page) {
-        //dirty hack for random table refresh interval (FORCE DELTAS)
-
-        List<Candidate> candidatesFromTable = page.getCandidatesFromTable();
-        int candidateTableHistorySize = candidateTableHistory.size();
-        if (candidateTableHistorySize > 0) {
-            for (int i = 0; i < 100; i++) {
-                if (candidatesFromTable.equals(
-                        candidateTableHistory.get(candidateTableHistorySize - 1))) {
-                    candidatesFromTable = page.getCandidatesFromTable();
-                } else {
-                    System.out.println("Force candidate table history delta tries: " + i);
-                    break;
-                }
-                if(i!= 0 && i%10 == 0){
-                    Driver.navigate().to("http://localhost:4200"); //try to refresh the page
-                }
-                if (i == 99) {
-                    throw new RuntimeException("Candidate table never updated with new candidate!");
-                }
-            }
-        }
-        return candidatesFromTable;
-    }
-
-    @Then("^diff tables to get the new candidates ID$")
-    public void diffTablesToGetNewID() {
-        candidateFromTableDiff = getCandidateFromTableDiff(
-                candidateTableHistory.get(0),
-                candidateTableHistory.get(1)
-        );
-    }
-
-    @Then("^the table does not contain the candidate$")
-    public void tableDoesNotContainCandidate() {
-        boolean containsDeleted = candidateTableHistory.get(2).contains(candidateFromTableDiff);
-        assert (!containsDeleted);
-    }
-
-
-    @And("^I enter a name to add$")
-    public void iEnterANameToAdd(List<Candidate> table)
-            throws Throwable {
-        scenarioDef.createNode(
-                new GherkinKeyword("And"),
-                "I enter a name to add");
-
-        CandidatePage page = new CandidatePage(Driver);
-
-        page.SendUserNameKeys(table.get(0).getName());
-
-        candidatesToAdd = table;
-    }
-
-
-    @And("^I enter the new candidates id to delete$")
-    public void iEnterTheNewCandidatesIDToDelete()
-            throws Throwable {
-        scenarioDef.createNode(
-                new GherkinKeyword("And"),
-                "I enter the new candidates id to delete");
-
-        CandidatePage page = new CandidatePage(Driver);
-
-        page.Delete(candidateFromTableDiff);
-
-    }
-
-    @And("^I click the add button$")
-    public void iClickTheAddButton() throws Throwable {
-        scenarioDef.createNode(new GherkinKeyword("And"),
-                "I click the add button");
-        CandidatePage page = new CandidatePage(Driver);
-        page.clickAdd();
-    }
-
-    @And("^I click the delete button$")
-    public void iClickTheDeleteButton() throws Throwable {
-        scenarioDef.createNode(new GherkinKeyword("And"),
-                "I click the delete button");
-        CandidatePage page = new CandidatePage(Driver);
-        page.clickDelete();
-    }
-
 }
